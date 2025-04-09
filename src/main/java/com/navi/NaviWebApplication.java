221f6e1b17803b03c2b4154723380d8025b5aabf
상희의보물\ehtkdgml11 <ehtkdgml11@gmail.com>
2025-04-09 22:41:52 +0900

    Init

---
+++ src/main/java/com/navi/NaviWebApplication.java
@@ -0,0 +1,13 @@
+package com.navi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navi/navi_web/NaviWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.navi_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
