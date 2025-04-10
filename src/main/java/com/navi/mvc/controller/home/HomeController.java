2b3ab34fbaebaf0d6c75e8c5befba76c020d5f47
ehdulwl-dev <ehdulwl@gmail.com>
2025-04-10 16:38:56 +0900

    FEATURE: home 컨트롤러 구현

--- src/main/java/com/navi/mvc/controller/home/HomeController.java
+++ src/main/java/com/navi/mvc/controller/home/HomeController.java
@@ -1,4 +1,18 @@
 package com.navi.mvc.controller.home;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
 public class HomeController {
+
+    @GetMapping( "/")
+    public String home() {
+        log.debug("홈 화면 이동 !");
+        return "views/home/home";
+    }
 }
